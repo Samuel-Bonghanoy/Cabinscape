7d66280430a1ebd3f45947961a3f5b57dcaa4f9e
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-06 12:23:45 +0800

    feat: configured react toast

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,15 @@
 // import { useEffect } from "react";
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 // import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -13,6 +18,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
