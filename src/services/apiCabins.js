5546d0f899da0ff696d4ede19a2dc61ac4d65506
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-06 23:35:40 +0800

    feat: added createCabin functionality

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,15 +12,31 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
   const { data, error } = await supabase
     .from("cabins")
-    .insert([{ newCabin }])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
   }
+
+  const avatarFile = event.target.files[0];
+  const { data, error } = await supabase.storage
+    .from("avatars")
+    .update("public/avatar1.png", avatarFile, {
+      cacheControl: "3600",
+      upsert: true,
+    });
+
+  return data;
 }
 
 export async function deleteCabin(id) {
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://liyjuqvvmfmsnyshsoug.supabase.co";
+export const supabaseUrl = "https://liyjuqvvmfmsnyshsoug.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpeWp1cXZ2bWZtc255c2hzb3VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0NjcyNDUsImV4cCI6MjAxMjA0MzI0NX0.x8qsyasCZSEpEh8dAh2cS5DU-I3GP4xe1jYWvF59a_Q";
 const supabase = createClient(supabaseUrl, supabaseKey);
