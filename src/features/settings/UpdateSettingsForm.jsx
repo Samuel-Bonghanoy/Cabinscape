0cbc6d32ce7da4a2e44dadf3a5086919dce6275a
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-07 13:55:48 +0800

    feat: refactored updateSetting into its own file

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -42,13 +42,21 @@ function UpdateSettingsForm() {
       </FormRow>
 
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          disabled={isUpdating}
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
+        />
       </FormRow>
 
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
           defaultValue={maxGuestsPerBooking}
         />
       </FormRow>
@@ -57,6 +65,8 @@ function UpdateSettingsForm() {
         <Input
           type="number"
           id="breakfast-price"
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
           defaultValue={breakfastPrice}
         />
       </FormRow>
