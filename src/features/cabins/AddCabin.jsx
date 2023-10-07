c1cb35aeca142d109e1d4df9a2e5b061fba9f1db
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-08 00:47:42 +0800

    feat: refactored addCabin to its own file

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,14 +1,18 @@
-import Button from 'ui/Button';
-import Modal from 'ui/Modal';
-import CreateCabinForm from './CreateCabinForm';
+import Button from "ui/Button";
+import Modal from "ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   return (
+    // <Button onClick={() => setShowForm((show) => !show)}>
+    //       Add new Cabin
+    //     </Button>
+    // {showForm && <CreateCabinForm />}
     <Modal>
-      <Modal.Toggle opens='new-cabin'>
+      <Modal.Toggle opens="new-cabin">
         <Button>Add new cabin</Button>
       </Modal.Toggle>
-      <Modal.Window name='new-cabin'>
+      <Modal.Window name="new-cabin">
         <CreateCabinForm />
       </Modal.Window>
     </Modal>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,14 +1,15 @@
 // import { useEffect } from "react";
-import { useState } from "react";
+// import { useState } from "react";
+import AddCabin from "../features/cabins/AddCabin";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+// import Button from "../ui/Button";
+// import CreateCabinForm from "../features/cabins/CreateCabinForm";
 // import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
+  // const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -18,10 +19,7 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new Cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
